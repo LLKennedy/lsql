2f06a397e8500ee81fc05a55aad195e2d16457e4
Luke Kennedy <luke.l.kennedy@gmail.com>
2020-10-19 17:18:13 +1100

    Generate go code

--- buildProto.ps1
+++ buildProto.ps1
@@ -3,5 +3,6 @@ $IncludeRule = "*.proto"
 $ExcludeRUle = [Regex]'.*google.*'
 $ProtoFiles = Get-ChildItem -path $Directory -Recurse -Include $IncludeRule | Where-Object FullName -NotMatch $ExcludeRUle
 foreach ($file in $ProtoFiles) {
-    protoc --proto_path="$($file.DirectoryName)" --go_out=plugins=grpc:"$($file.DirectoryName)" $file.FullName
+    protoc --proto_path="$($file.DirectoryName)" --go_out=paths=source_relative:./proto/golsql --go-grpc_out=paths=source_relative:./proto/golsql $file.FullName
 }
+go build ./proto/golsql
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/llkennedy/lsql
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.4.0
+	google.golang.org/grpc v1.33.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.0.0 // indirect
+)
