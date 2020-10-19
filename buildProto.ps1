054ed976ebed577d90615e5348a75fff627ce5d8
Luke Kennedy <luke.l.kennedy@gmail.com>
2020-10-19 17:38:10 +1100

    Ensure correct install of protoc-gen-go-grpc

--- buildProto.ps1
+++ buildProto.ps1
@@ -1,3 +1,5 @@
+go get google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.0.0
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
 $Directory = "."
 $IncludeRule = "*.proto"
 $ExcludeRUle = [Regex]'.*google.*'
@@ -6,3 +8,4 @@ foreach ($file in $ProtoFiles) {
     protoc --proto_path="$($file.DirectoryName)" --go_out=paths=source_relative:./proto/golsql --go-grpc_out=paths=source_relative:./proto/golsql $file.FullName
 }
 go build ./proto/golsql
+go mod tidy
