be31d44cb30add46964e1c3d3073e29ce4a7351b
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-01-19 21:05:56 +1100

    Fix build and import process

---
+++ @lsql/ui/src/exports.ts
@@ -0,0 +1,6 @@
+export * as core from "@lsql/core";
+export * as proto from "@lsql/proto";
+export * from "./query/classdefs";
+export * from "./query/QueryBuilder";
+export * from "./query/GroupBuilder";
+export * from "./query/FieldBuilder";
--- @lsql/ui/src/lib/exports.ts
+++
@@ -1,6 +0,0 @@
-export * as core from "@lsql/core";
-export * as proto from "@lsql/proto";
-export * from "../query/classdefs";
-export * from "../query/QueryBuilder";
-export * from "../query/GroupBuilder";
-export * from "../query/FieldBuilder";
