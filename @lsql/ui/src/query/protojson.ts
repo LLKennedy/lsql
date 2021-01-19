9a59ede3f18cb3a71468f8a1fbdaa20718e123c5
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-01-19 19:19:08 +1100

    Finish protojson marshalling

--- @lsql/ui/src/query/protojson.ts
+++ @lsql/ui/src/query/protojson.ts
@@ -1,78 +1,163 @@
-import { Ordering, UIGroup, Comparator, GroupOperator } from './where';
+import { Ordering, UIGroup, Comparator, GroupOperator, UIField, fieldWhereType, groupWhereType, PropertyType } from './where';
 import * as uuid from 'uuid';
+import { base64 } from 'rfc4648';
 
 export interface Paging {
-    limit?: number;
-    offset?: number;
+	limit?: number;
+	offset?: number;
 }
 
 export interface Group {
-    elements?: Element[];
-    operator?: GroupOperator;
-    negateOperator?: boolean;
+	elements?: Element[];
+	operator?: GroupOperator;
+	negateOperator?: boolean;
 }
 
 export interface StringValue {
-    stringValue?: string;
+	stringValue?: string;
 }
 
 export interface Int64Value {
-    int64Value?: number;
+	int64Value?: number;
 }
 
 export interface Uint64Value {
-    uint64Value?: number;
+	uint64Value?: number;
 }
 
 export interface DoubleValue {
-    doubleValue?: number;
+	doubleValue?: number;
 }
 
 export interface BoolValue {
-    boolValue?: boolean;
+	boolValue?: boolean;
 }
 
 export interface BytesValue {
-    bytesValue?: string;
+	bytesValue?: string;
 }
 
 export interface TimeValue {
-    /**ISO string */
-    timeValue?: string;
+	/**ISO string */
+	timeValue?: string;
 }
 
 export interface BaseField {
-    fieldName?: string;
-    negateComparator?: boolean;
-    comparator?: Comparator;
-    ordering?: Partial<Ordering>
+	fieldName?: string;
+	negateComparator?: boolean;
+	comparator?: Comparator;
+	ordering?: Partial<Ordering>
 }
 
-export type Field = BaseField & (StringValue | Int64Value | Uint64Value | DoubleValue | BoolValue | BytesValue | TimeValue)
+export type FieldValues = StringValue | Int64Value | Uint64Value | DoubleValue | BoolValue | BytesValue | TimeValue
+
+export type Field = BaseField & FieldValues
 
 export interface GroupElement {
-    group: Group;
+	group: Group;
 }
 
 export interface FieldElement {
-    field: Field;
+	field: Field;
 }
 
 export type Element = GroupElement | FieldElement;
 
 export interface Query {
-    id?: string;
-    paging?: Paging;
-    where?: Group;
+	id?: string;
+	paging?: Paging;
+	where?: Group;
 }
 
 /** Format expected by protojson */
 export function ToProto(group: UIGroup, paging?: Paging): Query {
-    let q: Query = {
-        id: uuid.v4()
-    }
-    if (paging !== undefined) {
-        q.paging = paging;
-    }
-    return q;
+	let q: Query = {
+		id: uuid.v4(),
+		where: groupToProto(group)
+	};
+	if (paging !== undefined) {
+		q.paging = paging;
+	}
+	return q;
+}
+
+function groupToProto(group: UIGroup): Group {
+	let g: Group = {
+		operator: group.operator,
+		negateOperator: group.negateOperator
+	};
+	if (group.elements?.length > 0) {
+		g.elements = [];
+		for (let i = 0; i < group.elements.length; i++) {
+			let element = group.elements[i];
+			switch (element.whereType) {
+				case fieldWhereType:
+					g.elements.push({
+						field: fieldToProto(element)
+					});
+					break;
+				case groupWhereType:
+				default:
+					g.elements.push({
+						group: groupToProto(element)
+					});
+					break;
+			}
+		}
+	}
+	return g;
+}
+
+function fieldToProto(field: UIField): Field {
+	let f: BaseField = {
+		comparator: field.comparator,
+		negateComparator: field.negateComparator,
+		fieldName: field.fieldName,
+		ordering: {
+			descending: field.ordering.descending,
+			priority: field.ordering.priority
+		}
+	};
+	let value: FieldValues;
+	switch (field.type) {
+		case PropertyType.BOOL:
+			value = {
+				boolValue: field.value
+			};
+			break;
+		case PropertyType.BYTES:
+			value = {
+				bytesValue: base64.stringify(field.value)
+			};
+			break;
+		case PropertyType.DOUBLE:
+			value = {
+				doubleValue: field.value
+			};
+			break;
+		case PropertyType.INT64:
+			value = {
+				int64Value: field.value
+			};
+			break;
+		case PropertyType.STRING:
+			value = {
+				stringValue: field.value
+			};
+			break;
+		case PropertyType.TIME:
+			value = {
+				timeValue: field.value.toISOString()
+			};
+			break;
+		case PropertyType.UINT64:
+			value = {
+				uint64Value: field.value
+			};
+			break;
+	}
+	return {
+		...f,
+		...value
+	};
 }
