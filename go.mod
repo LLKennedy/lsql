9a398d6c5a6fd2d6eb7920bf45b22b6a9e4cc5f4
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-01-19 18:10:52 +1100

    Marshal example message

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/golang/protobuf v1.4.3
+	github.com/google/uuid v1.1.2
 	google.golang.org/grpc v1.34.0
 	google.golang.org/protobuf v1.25.0
 )
--- golsql/internal/marshaltest/main.go
+++ golsql/internal/marshaltest/main.go
@@ -1,11 +1,123 @@
 package main
 
 import (
+	"log"
+
+	"github.com/google/uuid"
 	"github.com/llkennedy/lsql/golsql"
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
 	msg := &golsql.Query{}
+	msg.Id = uuid.New().String()
+	msg.Paging = &golsql.Paging{
+		Limit:  100,
+		Offset: 1000,
+	}
+	msg.Where = &golsql.WhereGroup{
+		NegateOperator: true,
+		Operator:       golsql.GroupOperator_OR,
+		Elements: []*golsql.WhereGroupElement{
+			{
+				Element: &golsql.WhereGroupElement_Field{
+					Field: &golsql.WhereField{
+						FieldName:        "Name",
+						NegateComparator: true,
+						Comparator:       golsql.Comparator_FUZZY_EQUAL,
+						Value: &golsql.WhereField_StringValue{
+							StringValue: "abcd",
+						},
+						Ordering: &golsql.Ordering{
+							Priority:   1,
+							Descending: true,
+						},
+					},
+				},
+			},
+			{
+				Element: &golsql.WhereGroupElement_Group{
+					Group: &golsql.WhereGroup{
+						NegateOperator: false,
+						Operator:       golsql.GroupOperator_XOR,
+						Elements: []*golsql.WhereGroupElement{
+							{
+								Element: &golsql.WhereGroupElement_Field{
+									Field: &golsql.WhereField{
+										FieldName:        "Width",
+										NegateComparator: false,
+										Comparator:       golsql.Comparator_GREATER_THAN,
+										Value: &golsql.WhereField_DoubleValue{
+											DoubleValue: 889.531,
+										},
+										Ordering: &golsql.Ordering{
+											Priority:   3,
+											Descending: false,
+										},
+									},
+								},
+							},
+							{
+								Element: &golsql.WhereGroupElement_Group{
+									Group: &golsql.WhereGroup{
+										NegateOperator: true,
+										Operator:       golsql.GroupOperator_OR,
+										Elements: []*golsql.WhereGroupElement{
+											{
+												Element: &golsql.WhereGroupElement_Field{
+													Field: &golsql.WhereField{
+														FieldName:        "Name",
+														NegateComparator: true,
+														Comparator:       golsql.Comparator_FUZZY_EQUAL,
+														Value: &golsql.WhereField_StringValue{
+															StringValue: "abcd",
+														},
+														Ordering: &golsql.Ordering{
+															Priority:   1,
+															Descending: true,
+														},
+													},
+												},
+											},
+											{
+												Element: &golsql.WhereGroupElement_Group{
+													Group: &golsql.WhereGroup{
+														NegateOperator: false,
+														Operator:       golsql.GroupOperator_XOR,
+														Elements: []*golsql.WhereGroupElement{
+															{
+																Element: &golsql.WhereGroupElement_Field{
+																	Field: &golsql.WhereField{
+																		FieldName:        "Width",
+																		NegateComparator: false,
+																		Comparator:       golsql.Comparator_GREATER_THAN,
+																		Value: &golsql.WhereField_DoubleValue{
+																			DoubleValue: 889.531,
+																		},
+																		Ordering: &golsql.Ordering{
+																			Priority:   3,
+																			Descending: false,
+																		},
+																	},
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 	data, err := protojson.Marshal(msg)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	log.Printf("%s", data)
 }
